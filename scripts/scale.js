7ab7f88d8e3361a3f984bd1934ed1f5e7aefd4c0
lir <344606398@qq.com>
2017-02-04 18:25:18 +0800

    update

--- scripts/scale.js
+++ scripts/scale.js
@@ -1,17 +1,19 @@
 //售电情况
 $(function() {
+    /*改变页面title值*/
+    $(".header_center").html("售电情况");
     var master = this.master = new TimelineMax({});
-
     master.add(saleMapStep());
     master.add(saleList());
+    master.add(saleListMap());
     master.add(saleListChange());
 
     function saleMapStep() {
         var step = new TimelineLite({
             title: "售电情况",
-            onComplete:function(){
-            	$("#sale_map .line").animate({opacity:"1"},3000);
-            	$("#sale_map .line_content").animate({opacity:"1"},3000);
+            onComplete: function() {
+                $("#sale_map .line").animate({ opacity: "1" }, 3000);
+                $("#sale_map .line_content").animate({ opacity: "1" }, 3000);
             }
         });
 
@@ -33,27 +35,35 @@ $(function() {
 
     function saleList() {
         var step = new TimelineLite({
-            title: "售电情况"
+            title: "售电情况",
+            onStart: function() {
+            	setInterval(function(){
+            		$(".sale_order li").animate({
+			            opacity: 0.25,
+			            height: "toggle"
+			        }, 1000, function() {
+			        	$(this).css("opacity",1);
+			            $(".sale_order li").slideDown(1000);
+			        });
+            	},5000);
+            	
+            }
         });
 
         var $obj = $("#sale_list");
 
         step.add(
-            TweenMax.fromTo($obj, 3, {
+            TweenMax.fromTo($obj, 1.5, {
                 opacity: 0,
-                left:500,
-                scale: 0
             }, {
                 opacity: 1,
-                scale: 1,
-                left:0
             })
         );
         return step;
     }
 
-    function saleListChange(){
-    	var step = new TimelineLite({
+    function saleListChange() {
+        var step = new TimelineLite({
             title: "售电情况"
         });
 
@@ -61,13 +71,44 @@ $(function() {
 
         step.add(TweenMax.fromTo($obj, 6, {
             opacity: 1,
-            delay:2
+            delay: 2
         }, {
             opacity: 0,
-            delay:2
+            delay: 2
         }));
         return step;
     }
 
+    function saleListMap() {
+        var step = new TimelineLite({
+            title: "售电情况"
+            // onStart:function(){
+            // 	setInterval(function(){
+	           //  	$("#sale_list_map .sale_list_data").animate({
+	           //  		opacity: 0.25,
+				        // height: "toggle"
+	           //  	},1000,function(){
+	           //  		$(this).css("opacity",1);
+				        // $("#sale_list_map .sale_list_data").slideDown(1000);
+	           //  	});
+            // 	},6000);
+            // }
+        });
+
+        var $obj = $("#sale_list_map");
+
+        step.add(
+            TweenMax.fromTo($obj, 1, {
+                opacity: 0,
+            }, {
+                opacity: 1,
+            })
+        );
+        return step;
+    }
+
     master.play();
+
+
+    
 })
--- scripts/summary.js
+++ scripts/summary.js
@@ -1,8 +1,9 @@
 //整体概况
 $(function() {
+	$(".header_center").html("整体概况");
     var master = this.master = new TimelineMax({});
     master.add(menuStep());
-    master.add(menuList3Step());
+    master.add([menuList2Step(),menuList3Step()]);
 
     function menuStep() {
         var step = new TimelineLite({
@@ -17,11 +18,39 @@ $(function() {
 
         step.add(TweenMax.fromTo($menu, 3, {
             opacity: 0,
-            right: -1200
+            right: -1200,
+            delay:10
         }, {
             opacity: 1,
-            left: 0
+            left: 0,
+            delay:10
         }));
+        return step;
+    }
+
+
+    function menuList2Step(){
+    	var step = new TimelineLite({
+            title: "市场预测",
+
+            onStart: function() {
+            	setInterval(function(){
+	            	$(".change").animate({
+	            		opacity: 0.25,
+				        height: "toggle"
+	            	},1000,function(){
+	            		$(this).css("opacity",1);
+				        $(".change").slideDown(1000);
+	            	});
+        		},5000);
+            }
+        });
+
+        var $obj = $(".change");
+
+        step.add(
+            TweenLite.to($obj, 2, { opacity:1})
+        );
         return step;
     }
 
@@ -29,17 +58,24 @@ $(function() {
         var step = new TimelineLite({
             title: "宏观经济",
             onStart: function() {
-                $("#scale").addClass("hide");
-                $("#company").addClass("hide");
+            	setInterval(function(){
+	            	$(".menu_list3>div").animate({
+	            		opacity: 0.25,
+				        height: "toggle"
+	            	},1000,function(){
+	            		$(this).css("opacity",1);
+				        $(".menu_list3>div").slideDown(1000);
+	            	});
+        		},5000);
             }
         });
 
         var $obj = $(".menu_list3");
 
 
-
+        /*rotationY: 360, transformOrigin: "center center", delay: 5*/
         step.add(
-            TweenLite.to($obj, 2, { rotationY: 360, transformOrigin: "center center", delay: 8 })
+            TweenLite.to($obj, 2, {opacity:1})
         );
         return step;
     }
